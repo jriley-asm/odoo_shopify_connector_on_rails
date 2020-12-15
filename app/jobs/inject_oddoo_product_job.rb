83a6a7064641d86e1ed69e0902d6e60404968e8c
Jack Riley <jriley9000@gmail.com>
2020-12-15 08:04:20 -0800

    product variants without ruby gem

--- app/jobs/inject_oddoo_product_job.rb
+++ app/jobs/inject_oddoo_product_job.rb
@@ -53,36 +53,6 @@ class InjectOddooProductJob < ApplicationJob
 
       puts shop_url
 
-      #header = {'Content-Type': 'text/json'}
-      product = {
-        "title" => "Burton Custom Freestyle 151"
-      }
-      puts "got here"
-
-      uri = URI(shop_url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      request = Net::HTTP::Get.new(uri)
-
-      request['X-Shopify-Access-Token'] = PASSWORD
-
-      #request = Net::HTTP::Get.new URI(shop_url)#, body: product)
-      response = http.request(request)
-      #request.each_header { |header| puts header }
-      #response =
-      puts "got there"
-      puts response.body
-
-      require 'shopify_api'
-
-      ShopifyAPI::Base.site = shop_url
-      ShopifyAPI::Base.api_version = "2020-10"
-
-      #puts new_product.options.size
-
-      new_product = ShopifyAPI::Product.new
-      new_product.title = product_rec_hash['name']
-      new_product.save
 
       odoo_product_atrribute_ids = product_rec_hash['attribute_line_ids']
 
@@ -90,131 +60,77 @@ class InjectOddooProductJob < ApplicationJob
       'product.template.attribute.line', 'read',
       [odoo_product_atrribute_ids])
 
-      puts new_product.options.size
-
-      new_product.options.delete_at(0)
-      #new_product.save
-
-
+      shopify_options_arr = []
 
       attributes.each do |attr|
         option_name = String(attr['display_name'])
-        puts option_name
-        new_product.options.append(ShopifyAPI::Option.new(:name => option_name))
-        #puts "Saved new option? #{option.save}"
-        puts 'here'
+        shopify_options_arr.append({"name": option_name})
       end
-      puts "saving product after initializing options: #{new_product.save}"
 
-      puts new_product.options.size
-      #puts "Saving product after adding options: #{new_product.save}"
-      #puts shopify_options_arr
-      #puts "Saved new product? #{new_product.save}"
-      #puts variants.class
+      shopify_variants_arr = []
+
       variants.each_with_index do |variant, variant_index|
-        puts variant['name']
-        #puts "current index: #{variant_index}"
-        variant_hash = ShopifyAPI::Variant.new(
-          #'weight' => variant['weight'],
-          'product_id' => new_product.id,
-          'price' => Float(variant['price']),#,
-          #'weight_unit' => variant['weight_uom_name'],
-          #'inventory_policy' => "deny",
-          #'inventory_management' => "shopify",
-          #'inventory_quantity' => Integer(variant['qty_available'])
-        )
-        #puts "variant has option 1? #{variant_hash.respond_to?(:option1)}"
-        #variant_hash.option1 = variant['name']
-        #puts "option1: #{variant_hash.option1}"
-        #puts variant_hash.price
+        this_variant_hash = {
+          'price' => Float(variant['price'])
+        }
 
+        #get sku if we can
         if variant['code'] != false
           puts 'thinks there is a variant hash code'
-          variant_hash.sku = variant['code']
-          puts "added sku: #{variant_hash.sku}"
+          this_variant_hash['sku'] = variant['code']
+          puts "added sku: #{this_variant_hash['sku']}"
         end
 
-        variant_hash.option1 = variant['name']
-
-        #puts 'first hash print: '
-        #puts JSON.pretty_generate(variant_hash)
-
-
-        #now we want to add the options (option1, option2 etc for shopify to this shopify variant hash)
-        #using the attributes associated with this ODOO product variant
         this_variant_attribute_ids = variant['attribute_line_ids']
 
         this_variant_attributes = models.execute_kw(db, uid, password,
         'product.attribute', 'read',
         [this_variant_attribute_ids])
-
-        #puts "attribute count: #{this_variant_attributes.length}"
-
+        puts 'got through variant attributes'
+        #go through the attributes for this product and give this variant each applicable option
         this_variant_attributes.each_with_index do |attr, index|
-          #we want to do two things in this loop, attatch each applicable option to the shopify variant hash
-          #and we want to pull the Odoo attribute values for this attribute/variant
-          #then we want to include those (or that one, because it should be an individual value in this case since we're dealing with a product variant.)
           product_attribute_values = models.execute_kw(db, uid, password,
-          'product.attribute.value', 'read',
-          [[attr['id']]])
+            'product.attribute.value', 'read',
+            [[attr['id']]])
 
-          if product_attribute_values.length > 1
-            puts 'TOO MANY ATTRIBUTE VALUES!!!'
-          elsif product_attribute_values.length == 1
-            puts 'pass'
-            #you can only define up to three options
-            new_option_name = product_attribute_values[0]['name']
-            puts new_option_name
-            case index
-            when 0
-              variant_hash.option1 = new_option_name
-              puts "set option 1"
-            # when 1
-            #   variant_hash.option2 = new_option_name
-            #   puts "set option 2: #{variant_hash.option2}"
-            # # title is our first option here
-            # when 2
-            #   variant_hash.option3 = new_option_name
-            #   puts "set option 3"
-            end
-          else
-            puts 'thought there were no attribute values for this attribute'
+          new_option_name = product_attribute_values[0]['name']
+          case index
+          when 0
+            this_variant_hash['option1'] = new_option_name
+          when 1
+            this_variant_hash['option2'] = new_option_name
+          when 2
+            this_variant_hash['option3'] = new_option_name
           end
         end
-        puts "Saved variant hash? #{variant_hash.save}"
-        #new_product.save
-        puts "about to share variant hash id"
-        puts "variant hash id: #{variant_hash.id}"
-        #new_product.variants << variant_hash
+        #need to check and make sure the variant's options are unique
+        if check_for_redundant_variant(shopify_variants_arr, this_variant_hash) == false
+          shopify_variants_arr.append(this_variant_hash)
+        end
+        #else do nothing
       end
 
+      product = {
+        "product" => {
+          "title" => product_rec_hash['name'],
+          "variants" => shopify_variants_arr,
+          "options" => shopify_options_arr
+        }
+      }
+      puts product
 
+      uri = URI(shop_url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Post.new(uri)
+      request.body = product.to_json
+      request['X-Shopify-Access-Token'] = PASSWORD
+      request['Content-Type'] = "application/json"
 
-      #need a list of product attributes
-      #attribute_line_ids from odoo
-      #this seems to be the way Odoo keeps track of product variant options
-      #we need to present a list of these options to shopify
-
-      puts 'got this far'
-
-      #puts 'ABOUT TO SHOW ATTRIBUTES:'
-
-      #puts JSON.pretty_generate(attributes)
-      #this needs to account for variations
-
-      #...
-      #save the new product and return whether or not we actually succeed
-      #puts shopify_options_arr
-
-      new_product.save
-      puts 'HERE'
-      puts new_product.id
-
-      if shopify_options_arr.length > 0
-        puts 'thinks it should save shopify product'
-        saved_product_shopify = new_product.save
-        puts "saved the product in shopify: #{saved_product_shopify}"
-      end
+      #request = Net::HTTP::Get.new URI(shop_url)#, body: product)
+      response = http.request(request)
+      puts response.body
+      response_hash = JSON.parse(response.body)
       #saved_product_shopify = false
 
       if saved_product_shopify
@@ -228,6 +144,48 @@ class InjectOddooProductJob < ApplicationJob
     else
       #this product already exists, we want to go ahead and update this product now
       puts 'we should update this product'
+    end
+  end
+
+  private
+  #returns true if there is a redundant var
+  #returns false if...well just apply binary logic
+  #i hate this code but i dont see an alternative
+  def check_for_redundant_variant(variants, variant_to_check)
+    variants.each do |curr_variant|
+      curr_variant_option_count = 0
+      #first we want to figure out how many options are we checking
+      if variant_to_check.key?("option1")
+        curr_variant_option_count = 1
+      end
+
+      if variant_.key?("option1") && variant.key?("option2")
+        curr_variant_option_count = 2
+      end
+
+      if variant.key?("option1") && variant.key?("option2") && variant.key?("option3")
+        curr_variant_option_count = 3
+      end
+
+      case curr_variant_option_count
+      when 1
+        if curr_variant['option1'] == variant_to_check['option1']
+          #we know then that we have found a redundant variant
+          return true
+        end
+      when 2
+        if curr_variant['option1'] == variant_to_check['option1'] && curr_variant['option2'] == variant_to_check['option2']
+          #we know then that we have found a redundant variant
+          return true
+        end
+      when 3
+        if curr_variant['option1'] == variant_to_check['option1'] && curr_variant['option2'] == variant_to_check['option2'] && curr_variant['option3'] == variant_to_check['option3']
+          #we know then that we have found a redundant variant
+          return true
+        end
+      else
+        return false
+      end
     end
   end
 end
